0e8ee37b2e6a48f15005b8d9358b7f942c8848c4
0nlyssslatts <sid36205@mail.ru>
2024-10-01 11:41:38 +0300

    scroll bar and done del buttons added dev

--- script/main.js
+++ script/main.js
@@ -2,13 +2,20 @@ const input = document.getElementById('input');
 const addButton = document.getElementById('addButton');
 const taskList = document.getElementById('taskList');
 
+
 addButton.onclick = function (){
-    taskList.insertAdjacentHTML(
-        'beforeend',
-        `
-        <li class = "task">
-            <span class = "task_text">${input.value}</span>
-        </li>
-        `
-    )
+
+    if(input.value.length != 0){
+        taskList.insertAdjacentHTML(
+            'beforeend',
+            `
+            <li class = "task">
+                <span class = "task_text">${input.value}</span>
+                <button class = "done_button">Done</button>
+                <button class = "del_button">Del</button>
+            </li>
+            `
+        )
+    }
+    
 }
