d743e505faddcd184c6a824398ae1520a9cad4f6
0nlyssslatts <sid36205@mail.ru>
2024-10-03 22:08:26 +0300

    adding removing and switching status dev

--- script/main.js
+++ script/main.js
@@ -3,19 +3,82 @@ const addButton = document.getElementById('addButton');
 const taskList = document.getElementById('taskList');
 
 
+const tasks = [
+    {
+        title:'Сделать',
+        completed: false
+    },
+    {
+        title:'Надо',
+        completed: true
+    }
+]
+
+function render() {
+    taskList.innerHTML = ''
+    for(let i = 0; i < tasks.length; i++) {
+        taskList.insertAdjacentHTML('beforeend', getTask(tasks[i], i))
+    }
+
+}
+
+function getTask(task, id){
+    return(
+        `
+        <li class = "task">
+
+            <span class = "task_text  ${task.completed ? 'done' : ''}">${task.title}</span>
+
+            <button 
+            class = "done_button${task.completed ? '_success' : ''}"
+            data-index="${id}"
+            data-type = "switch"
+            >Done</button>
+
+            <button 
+            class = "del_button" 
+            data-index="${id}"
+            data-type = "remove"
+            >Del</button>
+        </li>
+        `
+    )
+}
+
+
 addButton.onclick = function (){
 
     if(input.value.length != 0){
-        taskList.insertAdjacentHTML(
-            'beforeend',
-            `
-            <li class = "task">
-                <span class = "task_text">${input.value}</span>
-                <button class = "done_button">Done</button>
-                <button class = "del_button">Del</button>
-            </li>
-            `
-        )
+
+        const newTask = {
+            title: input.value,
+            completed: false
+        }
+
+        input.value = ''
+
+        tasks.push(newTask)
+
+        render()
     }
     
 }
+render()
+
+taskList.onclick = function(event){
+    //console.log(event.target.dataset)
+    if(event.target.dataset.index){
+        const index = Number(event.target.dataset.index)
+        const type = event.target.dataset.type
+
+        if(type === "switch"){
+            tasks[index].completed = !tasks[index].completed
+        }
+        else if (type === "remove"){
+            tasks.splice(index, 1)
+        }
+        render()
+    }
+}
+
+
